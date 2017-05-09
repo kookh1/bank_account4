5e0abe1ea96f53f9bd880e25fca0561402a369ef
koo <kookh1@naver.com>
2017-05-09 11:22:23 +0900

    [add] seperate member function from Account Class

--- bank_account3/Source.cpp
+++ bank_account3/Source.cpp
@@ -16,51 +16,63 @@ private:
 	int balance;         //고객의 잔액
 
 public:
-	Account(int accId, char *name, int balance)
-		: accId(accId), balance(balance)
-	{
-		this->name = new char[strlen(name) + 1];
-		strcpy(this->name, name);
-	}
-	
-	Account(const Account &ref)   //복사 생성자
-		: accId(ref.accId), balance(ref.balance)
-	{
-		this->name = new char[strlen(ref.name) + 1];
-		strcpy(this->name, ref.name);
-	}
+	Account(int accId, char *name, int balance);
+	Account(const Account &ref);
+	~Account();
 
-	~Account()
-	{
-		delete[] name;
-	}
-
-	int GetAccId() const //계좌ID 반환
-	{
-		return accId;
-	}
-	
-	void SetMoney(int money)   //입금 
-	{
-		this->balance += money;
-	}
-	
-	int GetMoney(int money)   //출금
-	{
-		if (money > this->balance)
-			return -1;
-
-		this->balance -= money;
-		return this->balance;
-	}
-
-	void ShowAccount() const  //계좌정보 보기
-	{
-		cout << "계좌ID: " << this->accId << endl;
-		cout << "이름: " << this->name << endl;
-		cout << "잔액: " << this->balance << endl << endl;
-	}
+	int GetAccId() const; //계좌ID 반환
+	void SetMoney(int money);   //입금 
+	int GetMoney(int money);   //출금
+	void ShowAccount() const;  //계좌정보 보기
 };
+
+
+Account::Account(int accId, char *name, int balance)
+	: accId(accId), balance(balance)
+{
+	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+}
+
+Account::Account(const Account &ref)   //복사 생성자
+	: accId(ref.accId), balance(ref.balance)
+{
+	this->name = new char[strlen(ref.name) + 1];
+	strcpy(this->name, ref.name);
+}
+
+Account::~Account()
+{
+	delete[] name;
+}
+
+int Account::GetAccId() const //계좌ID 반환
+{
+	return accId;
+}
+
+void Account::SetMoney(int money)   //입금 
+{
+	this->balance += money;
+}
+
+int Account::GetMoney(int money)   //출금
+{
+	if (money > this->balance)
+		return -1;
+
+	this->balance -= money;
+	return this->balance;
+}
+
+void Account::ShowAccount() const  //계좌정보 보기
+{
+	cout << "계좌ID: " << this->accId << endl;
+	cout << "이름: " << this->name << endl;
+	cout << "잔액: " << this->balance << endl << endl;
+}
+
+
 
 
 class AccountHandler
