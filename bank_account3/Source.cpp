3e9aa777dfcf5ef6ae6b5a1d03ff1c9a727244d3
koo <kookh1@naver.com>
2017-05-09 10:38:29 +0900

    [add] delete pointer array

--- bank_account3/Source.cpp
+++ bank_account3/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 /* 상수 정보 */
 const int NAME_LEN = 20;  //고객의 이름길이
 
-enum{MAKE=1, DEPOSIT, WITHDRAW, SHOW, EXIT};
+enum{MAKE=1, DEPOSIT, WITHDRAW, SHOW, EXIT}; //은행계좌 메뉴정보
 
 /*고객의 계좌정보*/
 class Account
@@ -178,6 +178,8 @@ int main()
 			ShowAllAccount(); //모든 계좌정보 메뉴 출력
 			break;
 		case EXIT:
+			for (int i = 0; i < accNum; i++)
+				delete accArr[i];
 			cout << "프로그램을 종료합니다." << endl;
 			return 0;
 		default:
